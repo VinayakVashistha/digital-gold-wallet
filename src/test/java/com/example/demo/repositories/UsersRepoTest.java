d8db5057e7492fde6279e404b85941397bdea707
garima <garima.7.2003@gmail.com>
2026-04-29 12:59:05 +0530

    repo test cases pakage created

---
+++ src/test/java/com/example/demo/repositories/UsersRepoTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class UsersRepoTest {
+	
+	@Disabled
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
+
